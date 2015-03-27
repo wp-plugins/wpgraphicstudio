78a40eb138b13ac167b7e248f6997328353c91d8
wpgraphicstudio <wpgraphicstudio@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-03-27 08:28:56 +0000

    update
    git-svn-id: https://plugins.svn.wordpress.org/wpgraphicstudio/trunk@1122027 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- includes/wpgs-nav-color-picker.js
+++
@@ -1,8 +0,0 @@
-(function( $ ) {
-
-    // Add Color Picker to all inputs that have 'color-field' class
-    $(function() {
-        $('.wpgs_wpgraphicstudio_nav_hex').wpColorPicker();
-    });
-
-})( jQuery );
