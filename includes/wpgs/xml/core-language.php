a82776371037d52d1f561c9979ba711c35dc80a1
wpgraphicstudio <wpgraphicstudio@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-05-22 19:16:12 +0000

    update
    git-svn-id: https://plugins.svn.wordpress.org/wpgraphicstudio/trunk@1165935 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ includes/wpgs/xml/core-language.php
@@ -0,0 +1,89 @@
+<?php
+$xmlstr = <<<XML
+<?xml version="1.0" encoding="UTF-8" standalone="yes"?>
+<langs>
+<langu>
+<navText>Text</navText>
+<navStyle>Style</navStyle>
+<navIcon>Icons</navIcon>
+<navColor>Colors</navColor>
+<navGraphics>Graphics</navGraphics>
+<font1>Arial Black</font1>
+<font2>Helvetica Neu</font2>
+<font3>Capture It</font3>
+<font4>Georgia</font4>
+<font5>Verdana</font5>
+<font6>Tekton Pro</font6>
+<font7>Stencil</font7>
+<font8>Impact</font8>
+<font9>League Gothic</font9>
+<font10>Lobster One</font10>
+<font11>Rosewood</font11>
+<font12>Myriad Pro</font12>
+<font13>Myriad Pro</font13>
+<textSaveOptions>Save Options</textSaveOptions>
+<textColorOptions>Color Options</textColorOptions>
+<textImageDimensions>Image Dimension</textImageDimensions>
+<textSaveAs>Save As</textSaveAs>
+<textIcon>Icon</textIcon>
+<textBackground>Background</textBackground>
+<textBackgroundColor>Background Color</textBackgroundColor>
+<textSaveTo>Save To</textSaveTo>
+<txtAlign>Align</txtAlign>
+<txtField>Text Field</txtField>
+<txtColor>Color</txtColor>
+<txtSize>Size</txtSize>
+<txtFont>Font</txtFont>
+<txtButton>Button Text</txtButton>
+<txtCTAboxes>Special Offer - Today Only!</txtCTAboxes>
+<txt1BelcherBoxes>Special Offer - Today Only!</txt1BelcherBoxes>
+<txt2BelcherBoxes>Add To Cart</txt2BelcherBoxes>
+<txt3BelcherBoxes>Get This Daily Special</txt3BelcherBoxes>
+<txt4BelcherBoxes>* Get Instant access - even at 2am - Guaranteed!</txt4BelcherBoxes>
+<txt1WebBoxes>Just a Big Headline</txt1WebBoxes>
+<txt2WebBoxes>Benefit Here</txt2WebBoxes>
+<txt3WebBoxes>Learn More</txt3WebBoxes>
+<txt4WebBoxes>a smaller sub-headline</txt4WebBoxes>
+<txt5WebBoxes>Lorem Ipsum and Blah and Much More Text Would Go Here</txt5WebBoxes>
+<txt1Headlines>Attention All Struggling Internet Marketers</txt1Headlines>
+<txt2Headlines>&quot;Finally, You Can Start Selling A Product That Everyone Needs &amp; Wants. The Newbie Checklist Allows Anyone To Get Started Online Easily, It's Never Been So Easy!&quot;</txt2Headlines>
+<txt3Headlines>Grab Private Label Rights Today For Only $12 &amp; Keep 100% Of The Profits!</txt3Headlines>
+<txtAdditional>Additional Text Field</txtAdditional>
+<btnReset>Reset design area.</btnReset>
+<btnDownload>Save graphic to computer.</btnDownload>
+<btnCapture>Save graphic to gallery.</btnCapture>
+<btnUpload>Upload graphic.</btnUpload>
+<btnDelete>Delete object from design area.</btnDelete>
+<btnBack>Move object to the back.</btnBack>
+<btnFront>Move object to the front.</btnFront>
+<btnAlignLeft>Align text to the left</btnAlignLeft>
+<btnAlignCenter>Align text to the center</btnAlignCenter>
+<btnAlignRight>Align text to the right</btnAlignRight>
+<btnAddText>Add additional text field</btnAddText>
+<btnHelp>View Help Videos</btnHelp>
+<textBorderStroke>Border Stroke</textBorderStroke>
+<textTexture>Texture</textTexture>
+<textTextureBackground>Texture Background</textTextureBackground>
+<textPaymentBoxBackground>Payment Box Background</textPaymentBoxBackground>
+<textDoodle>Doodle</textDoodle>
+<textHighlight>Highlight</textHighlight>
+<textPayment>Payment</textPayment>
+<textBorders>Borders</textBorders>
+<textmWidth>Width</textmWidth>
+<textXHeight>X-Height</textXHeight>
+<textNotice>Max width 800px, Max height 400pxMin width 400px, Min height 100px</textNotice>
+<textButtonColor>Button Color</textButtonColor>
+<textBottomColor>Bottom Color</textBottomColor>
+<textButtonBorderColor>Border Button Color</textButtonBorderColor>
+<textBorderColor>Border Color</textBorderColor>
+<BelcherBoxHelpTitle>Belcher Box Module - Help Videos</BelcherBoxHelpTitle>
+<ButtonsHelpTitle>Buttons Module - Help Videos</ButtonsHelpTitle>
+<ctaBoxesHelpTitle>Call to Action Module - Help Videos</ctaBoxesHelpTitle>
+<HeadlinesHelpTitle>Headlines Module - Help Videos</HeadlinesHelpTitle>
+<WebBoxHelpTitle>Web Boxes Module- Help Videos</WebBoxHelpTitle>
+<SelectHelpVideo>Select Help Video</SelectHelpVideo>
+<videoLoading>Video Loading: Please Standby</videoLoading>
+</langu>
+</langs>
+XML;
+?>
